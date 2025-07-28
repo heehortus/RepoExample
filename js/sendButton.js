b29a9642c42aebcdbb6abeb080b9cb30d0263597
HeeWon <hee.hortus@gmail.com>
2025-07-28 16:35:53 +0900

    feat: update form update submit js

---
+++ js/sendButton.js
@@ -0,0 +1,14 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // 기본 제출 막기
+
+    const name = document.querySelector('.contactTitle').value.trim();
+    const email = document.querySelector('.contactEmail').value.trim();
+    const message = document.querySelector('.contactMessage').value.trim();
+
+    if (name && email && message) {
+        alert("메시지가 성공적으로 전송되었습니다!");
+        window.location.reload(); // 페이지 새로고침
+    } else {
+        return false; // 제출 방지
+    }
+});
